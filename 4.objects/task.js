b238f89a8189b8d01cd937dd729514d4ebd19584
vymat <vymatvienko@gmail.com>
2023-03-14 13:53:28 +0300

    Четвертое задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
+"use strict"
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
+
+new Student("Vladimir");
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if ("marks" in this) {
+    return this.marks.push(...marksToAdd);
+  } 
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks.length === 0 || !("marks" in this)) {
+    return 0;
+  } else {
+    return this.marks.reduce((acc, number) => acc + number, 0) / this.marks.length;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
