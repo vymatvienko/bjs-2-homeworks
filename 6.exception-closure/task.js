1998fc291d171c461a387b11167e931381e29fc1
vymat <vymatvienko@gmail.com>
2023-03-28 13:06:40 +0300

    Шестое задание готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,52 @@
-﻿
+﻿"use strict"
+function parseCount(number) {
+    const value = Number.parseFloat(number);
+    if (isNaN(value)) {
+        throw new Error("Невалидное значение");
+    }
+    return value;
+}
+
+function validateCount(number) {
+    try {
+        return parseCount(number);
+    } catch(error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b < c || a + c < b || b + c < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.sideA = a;
+        this.sideB = b;
+        this.sideC = c;
+    }
+
+    get perimeter() {
+        return this.sideA + this.sideB + this.sideC;
+    }
+
+    get area() {
+        const p = (this.sideA + this.sideB + this.sideC) / 2;
+        return Number(Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC)).toFixed(3));
+    }
+}
+
+function getTriangle(a , b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch(error) {
+        return {
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            },
+
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
